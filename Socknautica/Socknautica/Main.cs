aa3ca3a454aa95cf3b2b833e672cd9f84fdb542a
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-10-25 20:20:22 -0400

    Create Main.cs

---
+++ Socknautica/Socknautica/Main.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using QModManager.API.ModLoading;
+using System.Reflection;
+
+namespace Socknautica
+{
+    [QModCore]
+    public static class Main
+    {
+        internal static Assembly assembly;
+        internal static Harmony harmony;
+
+        [QModPrePatch]
+        public static void Prepatch()
+        {
+            assembly = Assembly.GetExecutingAssembly();
+            harmony = new Harmony("Socksfor1.Socknautica");
+        }
+
+        [QModPatch]
+        public static void Patch()
+        {
+
+        }
+    }
+}
