a0926c03f458d0f0f32cc51b53f404b087a1f2f8
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-10-25 20:20:15 -0400

    Create OtherMods.cs

---
+++ Socknautica/Socknautica/OtherMods.cs
@@ -0,0 +1,21 @@
+﻿using QModManager.API;
+
+namespace Socknautica
+{
+    public static class OtherMods
+    {
+        private static bool? _submarineModExists;
+
+        public static bool SubmarineModExists
+        {
+            get
+            {
+                if (!_submarineModExists.HasValue)
+                {
+                    _submarineModExists = QModServices.Main.ModPresent("DADTankSubPack");
+                }
+                return _submarineModExists.Value;
+            }
+        }
+    }
+}
