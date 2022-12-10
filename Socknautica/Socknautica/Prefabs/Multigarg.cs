adfdd94f5b9007ed9d72addae2a0f7ecd77a363e
LeeTwentyThree <31892011+LeeTwentyThree@users.noreply.github.com>
2022-12-10 13:00:49 -0500

    improve multigarg AI

--- Socknautica/Socknautica/Prefabs/Multigarg.cs
+++ Socknautica/Socknautica/Prefabs/Multigarg.cs
@@ -16,9 +16,20 @@ internal class Multigarg : CreatureAsset
 
     public override EcoTargetType EcoTargetType => EcoTargetType.Whale;
 
+    public override float TurnSpeedHorizontal => 0.2f;
+    public override float TurnSpeedVertical => 3f;
+    public override float MaxVelocityForSpeedParameter => 40f;
+    public override RespawnData RespawnSettings => new RespawnData(false);
+
+    public override StayAtLeashData StayAtLeashSettings => new StayAtLeashData(0.4f, 400);
+    public override BehaviourLODLevelsStruct BehaviourLODSettings => new BehaviourLODLevelsStruct(400, 1000, 2000);
+
     public override void AddCustomBehaviour(CreatureComponents components)
     {
-        
+        components.swimRandom.swimForward = 0.7f;
+        components.locomotion.freezeHorizontalRotation = true;
+        components.locomotion.driftFactor = 0.5f;
+        components.locomotion.maxVelocity = 40f;
     }
 
     public override void SetLiveMixinData(ref LiveMixinData liveMixinData)
